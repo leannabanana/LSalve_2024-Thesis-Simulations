6761290ec3fd8336efa10ffbd418dc4f47eedcfd
leannabanana <109864570+leannabanana@users.noreply.github.com>
2024-05-04 19:19:53 +1000

    Organised methods and gev fitting/simulations
    04/05/2024

--- GEV_fitting.jl
+++ GEV_fitting.jl
@@ -1,63 +1,7 @@
+"""
+This file outputs simulations + fits GEV according to methods in "chaotic_system_methods.jl"
+"""
 include("chaotic_system_methods.jl")
-
-
-import Plots as pl
-using Plots, Extremes, Distributions, Gadfly, LaTeXStrings
-
-set_default_plot_size(25cm, 20cm)
-#CHAOS MAP
-function chaotic_map(a, n_steps)
-    y_values = Float64[]
-
-    for i in 0:1/n_steps:1
-        x = (a*i) % 1
-        push!(y_values, x)
-
-    end
-    return y_values
-end
-
-function T_mod_map(start, n)
-    y_values = Float64[]
-    x = start
-
-    for i in 0:n
-        x = (3*x) % 1
-        push!(y_values, x)
-
-    end
-    return y_values
-end
-
-function T_mod_map_noisey(start, n)
-    y_values = Float64[]
-    x = start
-    normal_dist_t = Normal(0, 1/(10^6))
-
-    for i in 0:n
-        x = (3*x) % 1 + rand(normal_dist_t)
-        push!(y_values, x)
-
-    end
-    return y_values
-end
-
-#Defining our observables
-function observable_one(vector_values, x0, alpha)
-    distances = abs.(vector_values .- x0).^(-alpha)
-    return distances
-end
-
-function observable_two(vector_values, x0)
-    distances = -log.(abs.(vector_values .- x0))
-    return distances
-end
-
-function observable_three(vector_values, x0, a, alpha)
-    distances = a.- (abs.(vector_values .- x0).^(-alpha))
-    return distances
-end
-
 
 alpha = 1/6
 x0 = 1/3
@@ -132,38 +76,6 @@ pl.plot(x_values, y, xlimits=(0,1), legend=false)
 p2 = pl.plot!(x_values, y_values_composed_2, mc =:pink, ms=2, ma=0.5, title=L"\phi = -log(d(x, x_0))")
 
 
-
 pl.scatter(x_values, y, xlimits=(0,1), label = L"T(x) = 3x \, \, mod \, \, 1", mc=:purple, legend=false)
 p3 = pl.plot!(x_values, y_values_composed_3, mc =:pink, ms=2, ma=0.5, title=L"\phi = c - d(x, x_0)^{-α}")
 
-
-using Plots
-
-# Define the map function
-T(x) = 3 * x % 1
-
-# Number of iterations
-N = 1000
-
-# Range of initial conditions
-x_range = collect(0:0.01:1)  # Adjust the step size for more or less granularity
-
-# Arrays to store the data points
-x_n = []
-x_nplus1 = []
-
-# Iterate the map for each initial condition
-for x₀ in x_range
-    x = x₀
-    for i in 1:N
-        x_next = T(x)
-        push!(x_n, x)
-        push!(x_nplus1, x_next)
-        x = x_next
-    end
-end
-
-# Plot x_{n+1} vs x_n
-scatter(x_n, x_nplus1, markersize=2, xlabel="xₙ", ylabel="xₙ₊₁", legend=false)
-pl.plot!(x_values, y)
-title!("Graph of xₙ₊₁ vs xₙ for T(x) = 3x mod 1 map")
--- chaotic_system_methods.jl
+++ chaotic_system_methods.jl
@@ -1,6 +1,64 @@
 """
 This file initialises methods required to simulate chaotic systems
 """
-import Plots as pl
+
+## Plots and Gadly are both plotting packages a plot command
+
+import Plots as pl #The Extremes package uses Gadfly as its plotting package this distinguishes packaes
 using Plots, Extremes, Distributions, Gadfly, LaTeXStrings
 
+set_default_plot_size(25cm, 20cm) ### Choosing a default plot size
+
+##### Simulating our Chaotic Map 
+function chaotic_map(a, n_steps)
+    y_values = Float64[]
+
+    for i in 0:1/n_steps:1
+        x = (a*i) % 1
+        push!(y_values, x)
+
+    end
+    return y_values
+end
+
+function T_mod_map(start, n)
+    y_values = Float64[]
+    x = start
+
+    for i in 0:n
+        x = (3*x) % 1
+        push!(y_values, x)
+
+    end
+    return y_values
+end
+
+### Perturbed System
+function T_mod_map_noisey(start, n)
+    y_values = Float64[]
+    x = start
+    normal_dist_t = Normal(0, 1/(10^6))
+
+    for i in 0:n
+        x = (3*x) % 1 + rand(normal_dist_t)
+        push!(y_values, x)
+
+    end
+    return y_values
+end
+
+#Defining observables
+function observable_one(x_values, x0, alpha)
+    distances = abs.(x_values .- x0).^(-alpha)
+    return distances
+end
+
+function observable_two(x_values, x0)
+    distances = -log.(abs.(x_values .- x0))
+    return distances
+end
+
+function observable_three(x_values, x0, a, alpha)
+    distances = a.- (abs.(x_values .- x0).^(-alpha))
+    return distances
+end
