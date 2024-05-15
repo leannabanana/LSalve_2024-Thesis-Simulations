020caa9d0a39e3ffd7ec4eacb9d660a8a4940d1b
leannabanana <109864570+leannabanana@users.noreply.github.com>
2024-05-15 10:17:09 +1000

    Added png diagnostic plots

--- GEV_fitting.jl
+++ GEV_fitting.jl
@@ -65,7 +65,7 @@ fit_obs3 = gevfit(maximums_3)
 d3 = diagnosticplots(fit_obs3)
 
 #Save Diagnostic Tests
-draw(PDF("Output_Images/gev_diagnostic_tests/obs1"*Date*".pdf", 25cm, 15cm), d1)
-draw(PDF("Output_Images/gev_diagnostic_tests/obs2"*Date*".pdf",25cm, 15cm), d2)
-draw(PDF("Output_Images/gev_diagnostic_tests/obs3"*Date*".pdf", 25cm, 15cm), d3)
+draw(PNG("Output_Images/gev_diagnostic_tests/obs1"*Date*".png", 25cm, 15cm), d1)
+draw(PNG("Output_Images/gev_diagnostic_tests/obs2"*Date*".png",25cm, 15cm), d2)
+draw(PNG("Output_Images/gev_diagnostic_tests/obs3"*Date*".png", 25cm, 15cm), d3)
 
