6b2807eaed4d68a5788313c15030d492fa0e2b02
leannabanana <leannasalve@gmail.com>
2024-07-05 15:35:48 +1000

    Theorem RV simulation verify

--- iid_param_sim.jl
+++ iid_param_sim.jl
@@ -8,19 +8,21 @@ function exponential_distributions(n_vectors, size, distribution)
     return random_vectors
 end
 
-num_vectors = 10
-vector_size = 10
-λ = 1.0
+num_vectors = 10^3
+vector_size = 10^3
+λ = 12
 distribution = Exponential(λ)
+
 X_n = exponential_distributions(num_vectors, vector_size, distribution)
-window_sizes = collect(1:4)
+window_sizes = collect(1:50)
+
 function rv_minimum(random_variables, window_sizes)
     shape_params = Float64[]
     location_params = Float64[]
     scale_params = Float64[]
     
     for windows in window_sizes
-        minimums = map(moving_minimum, random_variables, windows)
+        minimums = moving_minimum.(random_variables, windows)
         max_min = maximum.(minimums)
         
         shapes = 0.0
@@ -28,7 +30,7 @@ function rv_minimum(random_variables, window_sizes)
         scale_1 = 0.0
         
         try
-            fit = gevfit(max_min)
+            fit = gumbelfit(max_min)
             shapes = shape(fit)
             location_1 = location(fit)
             scale_1 = scale(fit)
@@ -49,4 +51,60 @@ function rv_minimum(random_variables, window_sizes)
     return shape_params, location_params, scale_params
 end
 
-rv_minimum(X_n, window_sizes)
+iid_case = rv_minimum(X_n, window_sizes)
+
+mus = pl.plot(window_sizes, iid_case[2], title=L"μ", legend=false)
+theta = pl.plot(window_sizes, iid_case[3], title=L"θ", legend=false)
+min_params_1 = pl.plot(mus, theta, size=(800, 600), layout=(1,2), plot_title="Simluated RV iid rv vs window size moving minimum")
+
+function rv_average(random_variables, window_sizes)
+    shape_params = Float64[]
+    location_params = Float64[]
+    scale_params = Float64[]
+    
+    for windows in window_sizes
+        minimums = moving_average.(random_variables, windows)
+        max_min = maximum.(minimums)
+        
+        shapes = 0.0
+        location_1 = 0.0
+        scale_1 = 0.0
+        
+        try
+            fit = gumbelfit(max_min)
+            shapes = shape(fit)
+            location_1 = location(fit)
+            scale_1 = scale(fit)
+        catch e
+            if isa(e, DomainError)
+                shapes = 0.0
+                location_1 = 0.0
+                scale_1 = 0.0
+            else
+                rethrow(e)
+            end
+        end
+
+        append!(shape_params, shapes)
+        append!(location_params, location_1)
+        append!(scale_params, scale_1)
+    end
+    return shape_params, location_params, scale_params
+end
+
+
+iid_case_av = rv_average(X_n, window_sizes)
+mus_av = pl.plot(window_sizes, iid_case_av[2],legend=false, ylabel=L"μ", xlabel=L"k")
+theta_av = pl.plot(window_sizes, iid_case_av[3], legend=false, ylabel=L"σ", xlabel=L"k")
+
+
+
+
+av_params = pl.plot(mus_av, theta_av, size=(800,600), layout=(1,2), plot_title="Simulated RV Moving Average Parameters vs Window Size")
+av_params_min = pl.plot(iid_case[2] .- iid_case[3].*log.(window_sizes), iid_case[2], legend=false, ylabel=L"\mu_k", xlabel=L"\mu", title=L"$\mu_k$ vs $\mu$ (moving minimum)")
+av_params_av = pl.plot(iid_case_av[2] .- iid_case_av[3].*log.(window_sizes), iid_case_av[2], legend=false, ylabel=L"\mu_k", xlabel=L"\mu", title=L"$\mu_k$ vs $\mu$  (moving average)")
+
+savefig(av_params,"Output_Images/verifying_dependent_iid_parameters/Moving_average_params_vs_windowsize.pdf")
+savefig(min_params_1,"Output_Images/verifying_dependent_iid_parameters/Moving_min_params_vs_windowsize.pdf")
+savefig(av_params_min,"Output_Images/verifying_dependent_iid_parameters/muk_vs_mu_movingmin.pdf")
+savefig(av_params_av,"Output_Images/verifying_dependent_iid_parameters/muk_vs_mu_movingav.pdf")
