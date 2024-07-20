be82e91410e0806a9d8fca01fa4f9704455d6ce6
leannabanana <leannasalve@gmail.com>
2024-07-20 11:43:43 +1000

    updated things

--- iid_param_sim.jl
+++ iid_param_sim.jl
@@ -207,7 +207,7 @@ function test_4(random_variables, window_size)
     location_params = Float64[]
     scale_params = Float64[]
 
-    for i in 10:size(random_variables)[1]
+    Threads.@threads for i in 10:size(random_variables)[1]
         minimums = moving_average_matrix(random_variables[1:i, :], window_size)
         max_min = maximum(minimums, dims=1)[:]
 
