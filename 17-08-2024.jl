e4c94dc22f35624dba2163c685bad4925375b493
leannabanana <leannasalve@gmail.com>
2024-08-19 10:28:17 +1000

    updated some plots

--- 17-08-2024.jl
+++ 17-08-2024.jl
@@ -11,8 +11,9 @@ pertubation = 1/10^3
 p0 = 0
 p1 = 1/(sqrt(2))
 
+
 orbits = simulate_orbits(initial_conditions, a, orbit_length, pertubation, n_orbits)
-average_orbits = observable_two.(orbits, 0)
+average_orbits = observable_two.(orbits, p1)
 mat_orb = reduce(hcat, average_orbits)
 estimate = EI_window_min(mat_orb, window_sizes)
 
@@ -21,14 +22,19 @@ CSV.write("Data_csv/updated_data/trajectories_1sqrt2.csv", dfmatrix)
 
 df = DataFrame(location = estimate[1], scale = estimate[2], EI = estimate[3], shape = estimate[4])
 CSV.write("Output_Images/18-08-2024/gevfit_min_1sqrt2_data.csv", df, delim=',', header=true)
-g1 = scatter(window_sizes, estimate[1], xticks=1:1:13, xlabel = " k ", ylabel = L"\mu", title=L"Moving minimum $x_0 = \dfrac{1}{\sqrt{2}}$", mc="tomato2", legend=false, ms=3, ma=1)
-pl.plot!(window_sizes, 0.5*(estimate[1][1]./ window_sizes))
-g2 = scatter(window_sizes, estimate[2], xticks=1:1:13, xlabel = " k ", ylabel = L"\sigma", title=L"Moving minimum $x_0 = \dfrac{1}{\sqrt{2}}$", mc="tomato2", legend=false, ms=3, ma=1)
+
+g1 = scatter(window_sizes, estimate[1], xticks=1:1:13, xlabel = " k ", ylabel = L"\mu", title=L"Moving minimum $x_0 = \dfrac{1}{\sqrt{2}}$", mc="tomato2",  ms=3, ma=1)
+pl.plot!(window_sizes, estimate[1][1].* 2 .^(-1.0 .* (window_sizes .- 1)), label=L"μ_2 = μ_1 λ^{-(k - 1)}")
+g2 = scatter(window_sizes, estimate[2], xticks=1:1:13, xlabel = " k ", ylabel = L"\sigma", title=L"Moving minimum $x_0 = \dfrac{1}{\sqrt{2}}$", mc="tomato2", ms=3, ma=1)
+pl.plot!(window_sizes, estimate[2][1].* 2 .^(-1.0 .* (exp.(2) .* window_sizes .- 1)), label=L"σ_2 = σ_1 λ^{\exp(2)k - 1}")
+pl.plot!(window_sizes, estimate[2][1].* 2 .^(-1.0 .* ( window_sizes .- 1)), label=L"σ_2 = σ_1 λ^{-(k - 1)}")
+
 g3 = scatter(window_sizes, estimate[3], xticks=1:1:13, xlabel = " k ", ylabel = L"\theta", title=L"Moving minimum $x_0 = \dfrac{1}{\sqrt{2}}$", mc="tomato2", legend=false, ms=3, ma=1)
 g4 = scatter(window_sizes, estimate[4], xticks=1:1:13, xlabel = " k ", ylabel = L"ξ", title=L"Moving minimum $x_0 = \dfrac{1}{\sqrt{2}}$", mc="tomato2", legend=false, ms=3, ma=1)
 
-gplot = pl.plot(g1, g2, g3, g4, layout = @layout([A B ; C D]), size=(700,550), plot_title="GEV Fit", plot_titlevspan=0.04)
-savefig(gplot,"Output_Images/18-08-2024/gevfit_min_1sqrt2.pdf")
+gplot = pl.plot(g1, g2, layout = @layout([A B]), size=(700,400), plot_title="GEV Fit", plot_titlevspan=0.04)
+
+savefig(gplot,"Output_Images/18-08-2024/mingevfit_lines_1sqrt2.pdf")
 
 
 orbits = simulate_orbits(initial_conditions, a, orbit_length, pertubation, n_orbits)
@@ -57,11 +63,19 @@ savefig(avfit,"Output_Images/18-08-2024/gevfit_av__1sqrt2.pdf")
 
 
 gumbel_av = gumbel_window_av(mat_orb, window_sizes)
-gm1 = scatter(window_sizes, gumbel_av[1], xticks=1:1:13, xlabel = " k ", ylabel = L"\mu", title=L"Moving av $x_0 = \dfrac{1}{\sqrt{2}}$", mc="tomato2", legend=false, ms=3, ma=1)
-gm2 = scatter(window_sizes, gumbel_av[2], xticks=1:1:13, xlabel = " k ", ylabel = L"\sigma", title=L"Moving av $x_0 = \dfrac{1}{\sqrt{2}}$", mc="tomato2", legend=false, ms=3, ma=1)
+gm1 = scatter(window_sizes, gumbel_av[1], xticks=1:1:13, legend=true, label="simulated", xlabel = " k ", ylabel = L"\mu", title=L"Moving av $x_0 = \dfrac{1}{\sqrt{2}}$", mc="tomato2", ms=3, ma=1)
+pl.plot!(window_sizes, gumbel_av[1][1] ./ log.(2 .* (window_sizes)), label = L"μ_2 = \dfrac{μ_1}{\log(2k)}") 
+pl.plot!(window_sizes, gumbel_av[1][1] ./ log.(2 .^ (window_sizes)), label = L"μ_2 = \dfrac{μ_1}{\log(2^k)}") 
+pl.plot!(window_sizes, gumbel_av[1][1] ./ window_sizes, label = L"μ_2 = \dfrac{μ_1}{k}")
+
+gm2 = scatter(window_sizes, gumbel_av[2], xticks=1:1:13, xlabel = " k ", ylabel = L"\sigma", title=L"Moving av $x_0 = \dfrac{1}{\sqrt{2}}$", mc="tomato2", legend = true, label = "simulated",  ms=3, ma=1)
+pl.plot!(window_sizes, gumbel_av[2][1] ./ window_sizes, label= L"σ_2 = \dfrac{σ_1}{k}")
+
 gm3 = scatter(window_sizes, gumbel_av[3], xticks=1:1:13, xlabel = " k ", ylabel = L"\theta", title=L"Moving av $x_0 = \dfrac{1}{\sqrt{2}}$", mc="tomato2", legend=false, ms=3, ma=1)
 
-gavfit = pl.plot(gm1, gm2, gm3, layout = @layout([A B ; C D]), size=(700,550), plot_title="Gumbel Fit", plot_titlevspan=0.04)
-savefig(gavfit,"Output_Images/18-08-2024/gumbelfit_av_1sqrt2.pdf")
+gavfit = pl.plot(gm1, gm2, layout = @layout([A B]), size=(700,400), plot_title="Gumbel Fit", plot_titlevspan=0.04)
+
+
+savefig(gavfit,"Output_Images/18-08-2024/gumbelfit_fits_1sqrt2.pdf")
 
 m4 = scatter(window_sizes, estimate_av[4], xticks=1:1:13, xlabel = " k ", ylabel = L"ξ", title=L"Moving av $x_0 = \dfrac{1}{\sqrt{2}}$", mc="tomato2", legend=false, ms=3, ma=1)
